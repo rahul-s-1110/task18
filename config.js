35f47d5e5300b15512ecef8a15b5f8e100b20736
rahul-s-1110 <saini1792@gmail.com>
2023-10-20 21:36:05 +0530

    proxy

--- config.js
+++ config.js
@@ -4,13 +4,7 @@ import { getFirestore} from 'firebase/firestore'
 
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyBQvB2ZZKzLodZg_2hw1v8wO4QaEQGkMjA",
-    authDomain: "thinktankerassignment.firebaseapp.com",
-    projectId: "thinktankerassignment",
-    storageBucket: "thinktankerassignment.appspot.com",
-    messagingSenderId: "363931064648",
-    appId: "1:363931064648:web:0ef93e60e6a5d44cc3bb7d",
-    measurementId: "G-RF5X1918LM"
+ 
   };
   
   const app = initializeApp(firebaseConfig);
